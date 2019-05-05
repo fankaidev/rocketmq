15eb24d6b904b5b938d55c8b6dd770ec8661ff73
fankai <fankai@fklj.net>
2019-05-05 08:58:09 +0800

    specifiy name server in demo

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -52,6 +52,8 @@ public class Consumer {
         /*
          * Specify where to start in case the specified consumer group is a brand new one.
          */
+        consumer.setNamesrvAddr("rocketmq-namesrv:9876");
+
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
         /*
